9992e44b5eb857b8f2bc99ed291230ef4dc040f9
xyin96 <yin530@gmail.com>
2015-02-25 00:35:27 -0500

    refactoring

--- doodle-jump.elm
+++ doodle-jump.elm
@@ -9,62 +9,69 @@ import List
 import Window
 
 -- Inputs
-type alias Input = { mouseX : Float, delta : Time }
+type Input = Click | MouseMove (Int, Int)
 
 delta = map inSeconds (fps 60)
 
 input : Signal Input
-input = sampleOn delta <| Input <~ map toFloat Mouse.x
-                                 ~ delta
+input = sampleOn delta (merge (map MouseMove Mouse.position) (map (always Click) Mouse.clicks))
 
 -- Model
 (gameWidth, gameHeight) = (600, 400)
 (halfWidth, halfHeight) = (300, 200)
 
-type alias Game = { bouncer : Bouncer, gameover : Bool }
-type alias Bouncer = { x : Float, y : Float, vx : Float, vy : Float, height : Float }
+type alias Game = { x : Float, y : Float, vx : Float, vy : Float, height : Float, gameOver : Bool }
 type alias Platform = { x : Float, y : Float, width : Float }
 
-character = foldp updateCharacter { x = 0, y = 0, vx = 0, vy = 5, height = 0 } input
+game = foldp update { x = 0, y = 0, vx = 0, vy = 5, height = 0, gameOver = False } input
 
 platforms = [{ x = halfWidth, y = 0, width = 100 }, { x = halfWidth / 2, y = 10, width = 100 },  { x = halfWidth * 3 / 2, y = -100, width = 100 }]
 
 -- Update
 
-updatePlatform : Bouncer -> Platform -> Platform
-updatePlatform bouncer platform = { x = platform.x, y = platform.y, width = platform.width }
 
-collision : Platform -> Bouncer -> Bouncer
+collision : Platform -> Game -> Game
 collision platform bouncer = {
   x = bouncer.x,
   y = bouncer.y,
   vx = bouncer.vx,
-  vy = (if | bouncer.vy < 0 && bouncer.x < platform.x + platform.width && bouncer.x > platform.x && bouncer.y < platform.y + 15 && bouncer.y > platform.y -> 10
-           | otherwise -> bouncer.vy),
-  height = bouncer.height}
+  vy = if | bouncer.vy < 0 && bouncer.x < platform.x + platform.width && bouncer.x > platform.x && bouncer.y < platform.y + 15 && bouncer.y > platform.y -> 10
+          | otherwise -> bouncer.vy,
+  height = bouncer.height,
+  gameOver = bouncer.gameOver}
 
-updateCharacter : Input -> Bouncer -> Bouncer
-updateCharacter { mouseX, delta } bouncer = List.foldr collision { 
-  x = bouncer.x - bouncer.vx,
-  y = bouncer.y + bouncer.vy,
-  vx = (bouncer.x - mouseX) / 5, 
-  vy = bouncer.vy - 0.5,
-  height = bouncer.height } platforms
+update : Input -> Game -> Game
+update input game = 
+  case input of
+    MouseMove (mouseX, mouseY) -> 
+      List.foldr collision { 
+        x = game.x - game.vx,
+        y = game.y + game.vy,
+        vx = (game.x - toFloat mouseX) / 5, 
+        vy = game.vy - 0.5,
+        height = game.height,
+        gameOver = if | game.y < -1 * halfHeight -> True
+                    | otherwise -> False
+      } platforms
+    Click -> if | game.gameOver -> { game | x <- halfWidth, y <- 0, vx <- 0, vy <- 10, gameOver <- False }
+                | otherwise -> game
 
---game : Signal Game
---game = foldp updatePlatforms character input
 
 -- Display
-display : (Int, Int) -> Bouncer -> Element
-display (w, h) {x, y, vx, vy} = 
+display : (Int, Int) -> Game -> Element
+display (w, h) g = 
+  let x = g.x
+      y = g.y
+      height = g.height in
   collage gameWidth gameHeight (List.append [
     rect gameWidth gameHeight |> filled (rgb 200 200 200),
     oval 15 15 |> filled (rgb 0 0 0) |> move (x - toFloat w / toFloat 2, y),
-    toForm (asText x) |> move (-50,-50)
+    if | g.gameOver -> toForm (asText "GameOver") |> move (150,150)
+       | otherwise -> toForm (asText "") |> move (0,0)
   ] (List.map renderPlatforms platforms))
   |> container w h middle
 
 renderPlatforms : Platform -> Form
 renderPlatforms platform = rect platform.width 15 |> filled (rgb 15 15 15) |> move (platform.x - toFloat halfWidth - platform.width / 2, platform.y)
 
-main = map2 display Window.dimensions character
+main = map2 display Window.dimensions game
